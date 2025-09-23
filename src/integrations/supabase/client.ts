c9be7b0d8c30ab6afb6d402aec59eddb425550b2
Alee Mendoza <176252672+altiora-software@users.noreply.github.com>
2025-09-22 21:33:50 -0300

    ipdate clients para variables de entorno

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,9 +4,16 @@ import type { Database } from './types';
 
 const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = process.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const url = import.meta.env.VITE_SUPABASE_URL as string
+const anon = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
-console.log('SUPABASE_PUBLISHABLE_KEY', SUPABASE_PUBLISHABLE_KEY);
-console.log('SUPABASE_URL', SUPABASE_URL);
+console.log('URL', url)
+console.log('ANON', anon)
+console.log('SUPABASE_URL', SUPABASE_URL)
+console.log('SUPABASE_PUBLISHABLE_KEY', SUPABASE_PUBLISHABLE_KEY)
+
+// Import the supabase client like this:
+// import { supabase } from "@/integrations/supabase/client";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
